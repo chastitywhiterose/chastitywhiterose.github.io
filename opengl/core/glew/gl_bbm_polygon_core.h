1b9a8062bfb6851de8d0af43f04ff1156abe6c61
Chastity White Rose <chastitywhiterose@gmail.com>
2021-02-28 16:24:30 -0600

    Add files via upload

---
+++ opengl/core/glew/gl_bbm_polygon_core.h
@@ -0,0 +1,163 @@
+/*
+ A version of my polygon functions that work only with core version 3.3 of OpenGL.
+ This version does not even have functions like glBegin,glEnd,glVertex2f, etc.
+This file is specifically only designed to be used in a program which has very specific shaders and the VAO and VBO set up in just a certain way. Each vertex is defined by 5 floats. The first two are for the X and Y coordinates and the next three are the red,green,and blue color values for that coordinate.
+I don't understand all of the code and will go back to https://open.gl/ to learn more about how it works.
+*/
+
+/*array to store the data in containing coordinates and colors.*/
+GLfloat polygon_vertex_array[0x1000];
+
+
+/*
+The following function creates the coordinates for the polygons but converts them to the normalized coordinates that opengl expects. The original opengl programs assumed an orthographic matrix such that the top left corner was X=0,Y=0 and the bottom right corner was X=width,Y=height. Modern OpenGL doesn't have this luxury since the "glOrtho" function from legacy OpenGL was removed from the API entirely. Instead I am left with the defaults that all coordinates are within the range -1 to 1. This also puts the center of the window as 0,0. This invalidated all my legacy OpenGL code but I came up with a workaround for this. If we divide the orthographic coordinates by the width and height we get the normalized coordinates. But adjusting the center was done but subtracting 0.5 from both X and Y.
+*/
+void get_polygon_points_core()
+{
+ double angle;
+ int i=0,i1=0;
+ while(i<polygon_sides)
+ {
+  angle=2*PI*i/polygon_sides+polygon_radians; 
+  polygon_vertex_array[i1]=(polygon_cx+sin(angle)*polygon_radius*2)/width-0.5; i1++;
+  polygon_vertex_array[i1]=(polygon_cy+cos(angle)*polygon_radius*2)/height-0.5; i1++;
+  polygon_vertex_array[i1]=color_r; i1++; /*red float*/
+  polygon_vertex_array[i1]=color_g; i1++; /*green float*/
+  polygon_vertex_array[i1]=color_b; i1++; /*blue float*/
+  i++;
+ }
+}
+
+
+/*
+this function does help get a regular polygon on the screen except that it may be stretched depending on the width to height ratio of the window.
+this was not intended behavior but it was the start of my experience with modern opengl and so I will preserve it because it worked for drawing something.
+*/
+
+void get_polygon_points_core1()
+{
+ double angle;
+ int i=0,i1=0;
+ while(i<polygon_sides)
+ {
+  angle=2*PI*i/polygon_sides+polygon_radians; 
+  polygon_vertex_array[i1]=/*polygon_cx+*/sin(angle)/**polygon_radius*/; i1++;
+  polygon_vertex_array[i1]=/*polygon_cy-*/cos(angle)/**polygon_radius*/; i1++;
+  polygon_vertex_array[i1]=color_r; i1++; /*red float*/
+  polygon_vertex_array[i1]=color_g; i1++; /*green float*/
+  polygon_vertex_array[i1]=color_b; i1++; /*blue float*/
+  i++;
+ }
+}
+
+/*filled regular polygon based on all the global variables*/
+void gl_polygon()
+{
+ get_polygon_points_core();
+ glBufferData(GL_ARRAY_BUFFER, sizeof(polygon_vertex_array), polygon_vertex_array, GL_DYNAMIC_DRAW);
+ glDrawArrays(GL_TRIANGLE_FAN,0,polygon_sides);
+}
+
+/*lines only, polygon is not filled*/
+void gl_polygon1()
+{
+ get_polygon_points_core();
+ glBufferData(GL_ARRAY_BUFFER, sizeof(polygon_vertex_array), polygon_vertex_array, GL_DYNAMIC_DRAW);
+ glDrawArrays(GL_LINE_LOOP,0,polygon_sides);
+}
+
+/*
+I finally learned how to use a function pointer!
+This function pointer allows me to change which function is currently being called each frame by various keypresses while the program is running. It defaults to the filled polygon function above but can be changed to anything else!
+*/
+void (*polyfunc)()=gl_polygon;
+
+/*
+ Drawing a filled star polygon is actually a lot more work than you might expect.
+ This is a modified form of my points getting function that fills up an array with enough points for multiple triangles.
+ The center x and y of the polygon are used as a corner of each triangle as well as two of the corners based on the step variable.
+*/
+void get_polygon_points_array_star()
+{
+ double angle;
+ int i=0,i1=0,ai=0;
+
+ while(i<polygon_sides)
+ {
+  i1=(i+polygon_step)%polygon_sides;
+
+  angle=2*PI*i/polygon_sides+polygon_radians; 
+  polygon_vertex_array[ai]=(polygon_cx+sin(angle)*polygon_radius*2)/width-0.5; ai++;
+  polygon_vertex_array[ai]=(polygon_cy+cos(angle)*polygon_radius*2)/height-0.5; ai++;
+  polygon_vertex_array[ai]=color_r; ai++;
+  polygon_vertex_array[ai]=color_g; ai++;
+  polygon_vertex_array[ai]=color_b; ai++;
+
+  angle=2*PI*i1/polygon_sides+polygon_radians; 
+  polygon_vertex_array[ai]=(polygon_cx+sin(angle)*polygon_radius*2)/width-0.5; ai++;
+  polygon_vertex_array[ai]=(polygon_cy+cos(angle)*polygon_radius*2)/height-0.5; ai++;
+  polygon_vertex_array[ai]=color_r; ai++;
+  polygon_vertex_array[ai]=color_g; ai++;
+  polygon_vertex_array[ai]=color_b; ai++;
+  
+  polygon_vertex_array[ai]=polygon_cx/width-0.5; ai++;
+  polygon_vertex_array[ai]=polygon_cy/height-0.5; ai++;
+  polygon_vertex_array[ai]=color_r; ai++;
+  polygon_vertex_array[ai]=color_g; ai++;
+  polygon_vertex_array[ai]=color_b; ai++;
+  i++;
+ }
+}
+
+void get_polygon_points_array_star_rgb()
+{
+ double angle;
+ int i=0,i1=0,ai=0;
+
+ while(i<polygon_sides)
+ {
+  i1=(i+polygon_step)%polygon_sides;
+
+  angle=2*PI*i/polygon_sides+polygon_radians; 
+  polygon_vertex_array[ai]=(polygon_cx+sin(angle)*polygon_radius*2)/width-0.5; ai++;
+  polygon_vertex_array[ai]=(polygon_cy+cos(angle)*polygon_radius*2)/height-0.5; ai++;
+  polygon_vertex_array[ai]=1; ai++;
+  polygon_vertex_array[ai]=0; ai++;
+  polygon_vertex_array[ai]=0; ai++;
+
+  angle=2*PI*i1/polygon_sides+polygon_radians; 
+  polygon_vertex_array[ai]=(polygon_cx+sin(angle)*polygon_radius*2)/width-0.5; ai++;
+  polygon_vertex_array[ai]=(polygon_cy+cos(angle)*polygon_radius*2)/height-0.5; ai++;
+  polygon_vertex_array[ai]=0; ai++;
+  polygon_vertex_array[ai]=1; ai++;
+  polygon_vertex_array[ai]=0; ai++;
+  
+  polygon_vertex_array[ai]=polygon_cx/width-0.5; ai++;
+  polygon_vertex_array[ai]=polygon_cy/height-0.5; ai++;
+  polygon_vertex_array[ai]=0; ai++;
+  polygon_vertex_array[ai]=0; ai++;
+  polygon_vertex_array[ai]=1; ai++;
+  i++;
+ }
+}
+
+
+/*
+filled star polygon. Uses the setup function above.
+*/
+void gl_polygon2()
+{
+ get_polygon_points_array_star_rgb();
+ glBufferData(GL_ARRAY_BUFFER, sizeof(polygon_vertex_array), polygon_vertex_array, GL_DYNAMIC_DRAW);
+ glDrawArrays(GL_TRIANGLES,0,polygon_sides*3);
+}
+
+/*
+uses GL_LINE_LOOP to draw the lines of the many triangles but not fill them.
+*/
+void gl_polygon3()
+{
+ get_polygon_points_array_star();
+ glBufferData(GL_ARRAY_BUFFER, sizeof(polygon_vertex_array), polygon_vertex_array, GL_DYNAMIC_DRAW);
+ glDrawArrays(GL_LINE_LOOP,0,polygon_sides*3);
+}
--- opengl/core/glew/main.c
+++ opengl/core/glew/main.c
@@ -1,1 +1,220 @@
-eee
+#include <stdio.h>
+#include <math.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int width=1280,height=720; /*size of window*/
+
+#define PI 3.14159265358979323846
+
+/*declare variables for attributes of the polygon*/
+int polygon_sides=3;         /*how many sides and/or corners of polygon*/
+int polygon_step=1;
+float polygon_cx;          /*horizontal left/right x coordinate of center*/
+float polygon_cy;         /*vertical up/down y coordinate of center*/
+float polygon_radius;     /*radius or how big the polygon will be*/
+
+float polygon_radians=0;           /*rotation of the polygon*/
+float polygon_radians_step=PI/180; /*how much the polygon rotates each frame*/
+
+float color_r=1,color_g=1,color_b=1;
+
+#include "gl_bbm_polygon_core.h"
+
+int frame=0;
+
+void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods)
+{
+ if(action==GLFW_PRESS || action==GLFW_REPEAT)
+ {
+  switch(key)
+  {
+   case GLFW_KEY_ESCAPE:
+    glfwSetWindowShouldClose(window,GLFW_TRUE);
+   break;
+   case GLFW_KEY_Q:
+    polyfunc=gl_polygon;
+   break;
+   case GLFW_KEY_W:
+    polyfunc=gl_polygon1;
+   break;
+  }
+ }
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int newwidth, int newheight)
+{
+ /*make sure the viewport matches the new window dimensions; note that width and 
+  height will be significantly larger than specified on retina displays.*/
+ width=newwidth;
+ height=newheight;
+ glViewport(0, 0, width, height);
+polygon_cx=width/2;
+polygon_cy=height/2;
+if(height<width){polygon_radius=height/3;}
+else{polygon_radius=width/3;}
+}
+
+const char *vertexShaderSource = "#version 330 core\n"
+"in vec2 position;\n"
+"in vec3 color;\n"
+"out vec3 Color;\n"
+"void main()\n"
+"{\n"
+ "Color=color;\n"
+"gl_Position=vec4(position,0.0,1.0);\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"in vec3 Color;\n"
+"out vec4 outColor;\n"
+"void main()\n"
+"{\n"
+"outColor=vec4(Color,1.0);\n"
+"}\n\0";
+
+/*variables defined separate from code for C90 purposes*/
+ GLFWwindow* window;
+ unsigned int vertexShader;
+ unsigned int fragmentShader;
+ unsigned int shaderProgram;
+ GLuint VBO, VAO;
+ GLint posAttrib;
+ GLint colAttrib;
+ int success;
+ char infoLog[512];
+
+int main()
+{
+
+ glfwInit();
+ glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+ glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+ glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+ glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+ window = glfwCreateWindow(width,height, "Chastity Modern OpenGL", NULL, NULL);
+ if (window == NULL)
+ {
+  printf("Failed to create GLFW window\n");
+  glfwTerminate();
+  return -1;
+ }
+
+ glfwMakeContextCurrent(window);
+
+
+glewExperimental=1;
+if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW\n");
+    return -1;
+}
+
+ glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);    
+ glfwSetKeyCallback(window,key_callback);
+
+ printf("The OpenGL version is: %s\n",glGetString(GL_VERSION));
+
+vertexShader = glCreateShader(GL_VERTEX_SHADER);
+ glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+ glCompileShader(vertexShader);
+
+
+
+ glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+ if (!success)
+ {
+  glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+  printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n",infoLog);
+ }
+fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+ glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+ glCompileShader(fragmentShader);
+
+ glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+ if (!success)
+ {
+  glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+  printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n",infoLog);
+ }
+ 
+shaderProgram = glCreateProgram();
+ glAttachShader(shaderProgram, vertexShader);
+ glAttachShader(shaderProgram, fragmentShader);
+ glLinkProgram(shaderProgram);
+
+ glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+ if(!success)
+ {
+  glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+  printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n",infoLog);
+ }
+ glDeleteShader(vertexShader);
+ glDeleteShader(fragmentShader);
+
+ glGenVertexArrays(1, &VAO);
+ glGenBuffers(1, &VBO);
+ glBindVertexArray(VAO);
+
+ glBindBuffer(GL_ARRAY_BUFFER, VBO);
+ 
+ /*position attributes*/
+ posAttrib = glGetAttribLocation(shaderProgram, "position");
+ glEnableVertexAttribArray(posAttrib);
+ glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE,5*sizeof(float), 0);
+
+ /*color attributes*/
+ colAttrib = glGetAttribLocation(shaderProgram, "color");
+ glEnableVertexAttribArray(colAttrib);
+ glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE,5*sizeof(float), (void*)(2*sizeof(float)));
+
+ glViewport(0,0,width,height);
+ glClearColor(0.0f,0.0f,0.0f,1.0f);
+
+ glfwSwapInterval(1);
+
+ polygon_cx=width/2;
+ polygon_cy=height/2;
+ polygon_radius=height/2;
+
+ glUseProgram(shaderProgram);
+
+ while (!glfwWindowShouldClose(window))
+ {
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  /*polyfunc();*/
+  gl_polygon2();
+  
+  polygon_radians+=PI/180;
+
+  frame++;
+  if(frame%360==0)
+  {
+   /*
+   This section increments the step of the star polygon. If the step would result in an invalid star polygon being drawn, the step is reset to 1 and the number of points incremented. The genius of   this is that it will cycle through every possible regular polygon both convex and star!
+  */
+  
+  polygon_step+=1;
+  if(polygon_step>=(polygon_sides/2)+polygon_sides%2)
+  {
+   polygon_sides+=1;
+   polygon_step=1;
+  }
+
+ }
+ 
+
+  glfwSwapBuffers(window);
+  glfwPollEvents();
+ }
+ 
+ glDeleteVertexArrays(1, &VAO);
+ glDeleteBuffers(1, &VBO);
+ glDeleteProgram(shaderProgram);
+
+ glfwTerminate();
+ return 0;
+}
+
+/*gcc -Wall -ansi -pedantic main.c -o main -lglfw -lOpenGL -lm -lGLEW && ./main*/
