8d53e1f085ffbf5c2cd95e72f2e13be0ddd9c786
Chastity White Rose <chastitywhiterose@gmail.com>
2021-02-28 16:23:24 -0600

    Add files via upload

--- opengl/ftgl/glut/main.c
+++ opengl/ftgl/glut/main.c
@@ -12,15 +12,22 @@ int frame_displayed=0;
 
 int chastity_ftgl_begin()
 {
- /*sprintf(fontname,"/usr/share/fonts/truetype/hack/Hack-Regular.ttf");*/ /*on my q4os installation*/
  sprintf(fontname,"/usr/share/fonts/TTF/Hack-Regular.ttf"); /*on my manjaro installation*/
  font = ftglCreatePixmapFont(fontname);
-
  if(!font)
  {
-  printf("Failed to load font \"%s\"!\n",fontname);
-  return 0;
+  printf("Failed to load first font \"%s\"!\n",fontname);
+  printf("Will try another one.\n");
+  
+  sprintf(fontname,"/usr/share/fonts/truetype/hack/Hack-Regular.ttf"); /*on my q4os installation*/
+  font = ftglCreatePixmapFont(fontname);
+  if(!font)
+  {
+   printf("Failed to load second font \"%s\"!\n",fontname);
+   return 0;
+  }
  }
+
  printf("Font \"%s\" loaded\n",fontname);
  
  ftglSetFontFaceSize(font,fontsize,0);
